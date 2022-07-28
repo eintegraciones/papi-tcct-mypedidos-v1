538dd0ac7da29b9a63aec080e85b28f14f5bdca2
Daniel Sanchez Fraile <dsanchfr@emeal.nttdata.com>
2022-07-28 18:31:24 +0200

    feat: logica de crear los schema de los xml

--- src/main/java/com/ntt/transformer/AvroSchema.java
+++ src/main/java/com/ntt/transformer/AvroSchema.java
@@ -7,6 +7,16 @@ import org.apache.avro.generic.GenericData;
 import org.apache.commons.io.FileUtils;
 import org.apache.parquet.hadoop.ParquetReader;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.StringReader;
+import org.xml.sax.InputSource;
+
 public class AvroSchema {
 
 	protected void writeSchema(String dataSchema, String nameSchema) {
@@ -20,5 +30,47 @@ public class AvroSchema {
         } catch(IOException e) {
             e.printStackTrace();
         } 
+	}
+	
+	
+	protected Document convertStringToXMLDocument(String xmlString) {
+		Document doc = null;
+		
+		//Parser that produces DOM object trees from XML content
+	    DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+	     
+	    //API to obtain DOM Document instance
+	    DocumentBuilder builder = null;
+	    try {
+	      //Create DocumentBuilder with default configuration
+	      builder = factory.newDocumentBuilder();
+	       
+	      //Parse the content to Document object
+	     doc = builder.parse(new InputSource(new StringReader(xmlString)));
+	    } catch (Exception e) {
+	      e.printStackTrace();
+	    }
+	    return doc;
+	}
+	
+	protected Element getNodeTable(Document docXML, String tableName) {
+		Element eElementTable = null;
+		Element eElementRoot = docXML.getDocumentElement();
+		NodeList nList = eElementRoot.getElementsByTagName("EntityType");
+        for (int temp = 0; temp < nList.getLength(); temp++) {
+            Node nNode = nList.item(temp);
+
+            //System.out.println("\nCurrent Element :" + nNode.getNodeName() + " Node.ELEMENT_NODE::"+  Node.ELEMENT_NODE);
+            if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+            	Element eElement = (Element)nNode;
+            	String name = eElement.getAttribute("Name");
+            	//System.out.println(name);
+            	if (tableName.equals(name)) {
+            		eElementTable = eElement;
+            	}
+            }
+
+        }
+		return eElementTable;
 	}
 }
--- src/main/java/com/ntt/transformer/AvroSchemaDataverse.java
+++ src/main/java/com/ntt/transformer/AvroSchemaDataverse.java
@@ -1,34 +1,75 @@
 package com.ntt.transformer;
 
-public class AvroSchemaDataverse extends AvroSchema implements AvroTraductor {
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
-	@Override
-	public void fileSchema(Object data, String tableName) {
-		String schema = "{\"type\" : \"record\", \"namespace\" : \"com.nttdata.bean\", \"name\" : \"" + tableName + "\", \"fields\" : [";
-		/*for(String key : map.keySet()) {
-			if(map.get(key) instanceof Double) {
-				schema = schema + "{\"name\" : \"" + key + "\", \"type\" : [\"null\", \"double\"] },";
-			} else if(map.get(key) instanceof Boolean) {
-				schema = schema + "{\"name\" : \"" + key + "\", \"type\" : [\"null\", \"boolean\"] },";
-			} else if(map.get(key) instanceof Integer) {
-				schema = schema + "{\"name\" : \"" + key + "\", \"type\" : [\"null\", \"int\"] },";
-			} else if(map.get(key) instanceof Float) {
-				schema = schema + "{\"name\" : \"" + key + "\", \"type\" : [\"null\", \"float\"] },";
-			} else if(map.get(key) instanceof Long) {
-				schema = schema + "{\"name\" : \"" + key + "\", \"type\" : [\"null\", \"long\"] },";
-			} else if(map.get(key) instanceof String) {
-				schema = schema + "{\"name\" : \"" + key + "\", \"type\" : [\"null\", \"string\"] },";
-			} else {
-				schema = schema + "{\"name\" : \"" + key + "\", \"type\" : [\"null\", \"string\"] },";
-			}
-		}*/
-		schema = schema.substring(0, schema.length() - 1);
-		schema = schema + "]}";
-		
-		
-		
-		writeSchema(schema, tableName + ".schame");
+public class AvroSchemaDataverse extends AvroSchema {
+
+	
+	public AvroSchemaDataverse() {
+		//System.out.println("Creo instancia");
 	}
+
+	public void fileSchema(String data, String tableName) {
+		StringBuilder schema = new StringBuilder();
+		
+		Document docXML = convertStringToXMLDocument(data);
+		docXML.getDocumentElement().normalize();
+		Element eElement = getNodeTable(docXML, tableName);
+		
+		schema.append("{\"type\" : \"record\", \"namespace\" : \"com.nttdata.bean\", \"name\" : \"" + tableName + "\", \"fields\" : [");
+		NodeList nListProperty =  eElement.getElementsByTagName("Property");
+		for (int temp = 0; temp < nListProperty.getLength(); temp++) {
+			Node nNode = nListProperty.item(temp);
+
+			if (temp > 0) {
+				schema.append(",");
+			}
+			
+			if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+				Element eElementProperty = (Element)nNode;
+				
+				String name = eElementProperty.getAttribute("Name"); 
+				schema.append("{\"name\":\"").append(name).append("\",");
+				
+				String type = eElementProperty.getAttribute("Type");
+				switch (type) {
+				case "Edm.String":
+					schema.append("\"type\":\"[\"null\",\"string\"]");
+					break;
+				case "Edm.Double":
+					schema.append("\"type\":\"[\"null\",\"double\"]");
+					break;
+				case "Edm.Boolean":
+					schema.append("\"type\":\"[\"null\",\"boolean\"]");
+					break;
+				case "Edm.Int32":
+					schema.append("\"type\":\"[\"null\",\"int\"]");
+					break;
+				case "Edm.Float":
+				case "Edm.Decimal":
+					schema.append("\"type\":\"[\"null\",\"float\"]");
+					break;
+				case "Edm.Int64":
+					schema.append("\"type\":\"[\"null\",\"long\"]");
+					break;
+				default:
+					schema.append("\"type\":\"[\"null\",\"string\"]");
+					break;
+				}
+				schema.append("}");
+			}
+		}
+		schema.append("]}");
+
+		System.out.println(schema.toString());
+		
+		writeSchema(schema.toString(), tableName + ".schame");
+	}
+
+	
 
 
 
